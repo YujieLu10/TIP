35e1f40ec970284ad01d41ed9e227e345332e60d
yujielu10 <yujielu10@gmail.com>
2023-05-02 16:33:16 -0700

    initial release

--- test.py
+++
@@ -1,8 +0,0 @@
-"""
-Download the weights in ./checkpoints beforehand for fast inference
-wget https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model*_base_caption.pth
-wget https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model*_vqa.pth
-wget https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base_retrieval_coco.pth
-"""
-
-
